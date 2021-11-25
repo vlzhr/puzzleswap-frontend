695817b65bc5789c710851fb2265bfbcbbf5ec51
mlayus <mlayus@web3tech.ru>
2021-11-25 16:05:26 +0200

    missing blocks added, footer

--- src/LandingModule.tsx
+++ src/LandingModule.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import landingImage from './img/hero-image-2x.png';
+import landingInvestImage from './img/landing-invest-image.png';
 import fomoLogo from "./img/fomo-logo.jpeg";
 import {appTokens, downloadStates} from "./Pools";
 import {calculateTokenPrice} from "./WalletModule";
@@ -9,6 +10,7 @@ import mediumLogo from "./img/medium-logo.svg";
 import {API_URL} from "./MultiSwapInterface";
 import adv1 from "./img/adv1.png";
 import adv2 from "./img/adv2.png";
+import priceIcon from "./img/price-icon.svg";
 import adv3 from "./img/adv3.png";
 import adv4 from "./img/adv4.png";
 import bannerToken from "./img/banner-token.png";
@@ -66,12 +68,12 @@ export class LandingModule extends React.Component<IProps, IState> {
         console.log(item)
         return <div className="table-body table-row">
             <div className="table-cell">
-                <img src={item.logo} alt="" className="landing__prices--logo" /><span>{item.name}</span>
+                <img src={item.logo} alt="img" className="landing__prices--logo" /><span>{item.name}</span>
             </div>
             <div className="table-cell">${item.price}</div>
             {/*<div className="table-cell">+ 115%</div>*/}
             <div className="table-cell">
-                <Link className="ignore-link" to={"/"+item.pool}><button className="button secondary medium">Trade</button></Link>
+                <button className="button secondary medium"><Link className="ignore-link" to={"/"+item.pool}>Trade</Link></button>
             </div>
         </div>
     }
@@ -86,11 +88,11 @@ export class LandingModule extends React.Component<IProps, IState> {
                     <Link to="/defi"><button className="button primary large">Trade</button></Link>
                 </div>
                 <div className="landing__intro--rc">
-                    <img className="landing__intro--rc-image" src={landingImage} alt=""/>
+                    <img className="landing__intro--rc-image" src={landingImage} alt="img"/>
                 </div>
                 <div className="landing__intro-more" onClick={() => window.open("https://medium.com/@izhur27/what-is-puzzle-swap-1e4b4af4ed17")}>
                     Learn more on
-                    <img src={mediumLogo} alt=""/>
+                    <img src={mediumLogo} alt="img"/>
                 </div>
             </div>
             <div className="landing__about">
@@ -98,28 +100,26 @@ export class LandingModule extends React.Component<IProps, IState> {
                 <h1 className="landing__title">Solving multiple pain points and market needs</h1>
                 <div className="landing__about--advantages">
                     <div className="landing__about--advantages-item">
-                        <img src={adv1} alt=""/>
+                        <img src={adv1} alt="img"/>
                         <h2>Mega pools</h2>
                         <p>Trade up to 10 tokens in one pool without extra fees and lower slippage risks. Any token can be exchanged to any other.</p>
                     </div>
                     <div className="landing__about--advantages-item">
-                        <img src={adv2} alt=""/>
+                        <img src={adv2} alt="img"/>
                         <h2>Portfolio pools</h2>
                         <p>Create a custom trading pool and provide liquidity from your portfolio with any token which is suitable for you.</p>
                     </div>
                     <div className="landing__about--advantages-item">
-                        <img src={adv3} alt=""/>
+                        <img src={adv3} alt="img"/>
                         <h2>Fair routing</h2>
                         <p>Suitable routing service between custom pools and AMM-exchanges on Waves — trade with the best fare.</p>
                     </div>
                     <div className="landing__about--advantages-item">
-                        <img src={adv4} alt=""/>
+                        <img src={adv4} alt="img"/>
                         <h2>Trading&nbsp;subscriptions</h2>
                         <p>Purchase subscriptions with PUZZLE to pay less fees if you are a regular user.</p>
                     </div>
                 </div>
-            </div>
-            <div className="landing__about--banner-wrapper">
                 <div className="landing__about--banner">
                     <div className="landing__about--banner-lc">
                         <div>
@@ -133,10 +133,11 @@ export class LandingModule extends React.Component<IProps, IState> {
                         </div>
                     </div>
                     <div className="landing__about--banner-rc">
-                        <img className="landing__about--banner-rc-image" src={bannerToken} alt=""/>
+                        <img className="landing__about--banner-rc-image" src={bannerToken} alt="img"/>
                     </div>
                 </div>
             </div>
+
 
             <div className="landing__traders">
                 <p className="landing__subtitle">Trade</p>
@@ -148,7 +149,10 @@ export class LandingModule extends React.Component<IProps, IState> {
 
                     <div className="table-head table-row">
                         <div className="table-cell">Asset</div>
-                        <div className="table-cell">Price</div>
+                        <div className="table-cell">
+                            <p>Price</p>
+                            <img src={priceIcon} alt="img"/>
+                        </div>
                         {/*<div className="table-cell">APY</div>*/}
                         <div className="table-cell"></div>
                     </div>
@@ -157,7 +161,7 @@ export class LandingModule extends React.Component<IProps, IState> {
 
                     {/*<div className="table-body table-row">*/}
                     {/*    <div className="table-cell">*/}
-                    {/*        <img src={fomoLogo} alt="" className="landing__prices--logo" /> FOMO*/}
+                    {/*        <img src={fomoLogo} alt="img" className="landing__prices--logo" /> FOMO*/}
                     {/*    </div>*/}
                     {/*    <div className="table-cell">$1440</div>*/}
                     {/*    <div className="table-cell">+ 115%</div>*/}
@@ -166,6 +170,23 @@ export class LandingModule extends React.Component<IProps, IState> {
                     {/*    </div>*/}
                     {/*</div>*/}
                 </div>
+            </div>
+
+            <div className="landing__invest">
+                <div className="landing__invest--lc">
+                    <p className="landing__subtitle">Invest</p>
+                    <h1 className="landing__title">Be one of the first liquidity providers<br/>in Puzzle MegaPools!</h1>
+                    <p className="landing__desc">
+                        Earn passive income for staking your portfolio and exchange fees from puzzle volume at the same time.
+                        <br/>
+                        <a href="https://collective.wavesducks.com" target="_blank"><strong>Learn more on our Medium</strong></a>.
+                    </p>
+                    <Link to="/defi"><button className="button primary large">Invest</button></Link>
+                </div>
+                <div className="landing__invest--rc">
+                    <img className="landing__invest--rc-image" src={landingInvestImage} alt="img"/>
+                </div>
+
             </div>
         </div>
     }
